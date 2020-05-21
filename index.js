ac65209df38b5a54231b20ae5dce3f48dfd09687
lokeshlk368 <lk689735@gmail.com>
2020-05-22 02:11:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Requrire the express
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log(`Error in running the server:{err}`);
+        return;
+    }
+    console.log(`Server is running with port:${port}`);
+})
